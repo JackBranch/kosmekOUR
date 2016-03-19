5c53653310e02d9239163642be5908040c8b211d
Jack Volontyr <soapforwork@gmail.com>
2016-03-19 18:52:42 +0200

    Begin.

---
+++ autocomplete/eng_autocomplete.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+
+<!--
+jQuery( function() {
+     jQuery( '#kyewaord' ) .autocomplete( {source : '../php_file/autocomplete.php'});
+} );
+// -->
---
+++ js/buildmenu.js
@@ -0,0 +1,2 @@
+//build menu with DIV ID="myslidemenu" on page:
+droplinemenu.buildmenu("droplinetabs1")
